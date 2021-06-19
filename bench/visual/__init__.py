2ce4918702a1b79c2cc4a8cfce61ddb190784bf5
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2021-06-19 16:20:36 +1000

    New bench results

---
+++ bench/visual/__init__.py
@@ -0,0 +1,57 @@
+import subprocess
+from typing import Tuple
+from IPython.display import Markdown, display
+import pandas as pd
+import platform
+import psutil
+import math
+
+class Pipeline:
+    @staticmethod
+    def load_results() -> pd.DataFrame:
+        return pd.read_csv('./_logs/results.csv')
+
+    @staticmethod
+    def mean_over_invocation(df: pd.DataFrame) -> Tuple[pd.DataFrame, int]:
+        df = df.copy()
+        benchmarks = set(df['bench'])
+        mallocs = set(df['malloc'])
+        # metrics = [c for c in df.columns if c not in ['bench', 'malloc', 'invocation']]
+        data = {}
+        invocations = 0
+        for index, row in df.iterrows():
+            key = (row['malloc'], row['bench'])
+            if key not in data: data[key] = []
+            data[key].append(index)
+        drop_indexes = []
+        for bm in benchmarks:
+            for a in mallocs:
+                idx = data[(a, bm)]
+                invocations = len(idx)
+                avg = df.loc[idx].mean(axis=0)
+                v = [v for i, v in avg.items()]
+                i = [i for i, v in avg.items()]
+                df.loc[idx[0], i] = v
+                drop_indexes += idx[1:]
+        df.drop(drop_indexes, inplace=True)
+        df.drop(['invocation'], axis=1, inplace=True)
+        df.reset_index(drop=True, inplace=True)
+        return (df, invocations)
+
+def markdown(s: str):
+    display(Markdown(s))
+
+def display_meta_info():
+    git_revision = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'], text=True).strip()
+    uname = platform.uname()
+    os = f'{uname.system} ({uname.release})'
+    cpu = subprocess.check_output("lscpu | grep 'Model name:' | sed -r 's/Model name:\s{1,}//g'", shell=True, text=True).strip()
+    mem = str(math.ceil(psutil.virtual_memory().total / (1024.0 ** 3))) + ' GB'
+    markdown(
+        f'| Meta          | Value          |\n'
+        f'|:------------- | --------------:|\n'
+        f'| Git revision  | {git_revision} |\n'
+        f'| System        | {os}           |\n'
+        f'| Processor     | {cpu}          |\n'
+        f'| Memory        | {mem}          |\n'
+    )
