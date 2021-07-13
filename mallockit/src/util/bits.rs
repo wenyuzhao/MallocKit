746bfedb69adcd1cf247c75e719681a70802c3f4
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2021-07-13 22:45:02 +1000

    Cleanup

--- mallockit/src/util/bits.rs
+++ mallockit/src/util/bits.rs
@@ -8,7 +8,17 @@ pub struct BitField {
 
 pub trait BitFieldSlot: Sized {
     fn get(&self, field: BitField) -> usize;
-    fn set(&self, field: BitField, value: usize);
+    fn set(&mut self, field: BitField, value: usize);
+    fn delta(&mut self, field: BitField, delta: isize) -> usize {
+        let old = self.get(field);
+        let new = if delta > 0 {
+            old + (delta as usize)
+        } else {
+            old - ((-delta) as usize)
+        };
+        self.set(field, new);
+        new
+    }
 }
 
 impl BitFieldSlot for AtomicUsize {
@@ -19,12 +29,41 @@ impl BitFieldSlot for AtomicUsize {
     }
 
     #[inline(always)]
-    fn set(&self, field: BitField, value: usize) {
+    fn set(&mut self, field: BitField, value: usize) {
         let old_value = self.load(Ordering::Relaxed);
         let mask = ((1usize << field.bits) - 1) << field.shift;
         let shifted_value = value << field.shift;
         debug_assert!((shifted_value & !mask) == 0);
         let new_value = (old_value & !mask) | (value << field.shift);
         self.store(new_value, Ordering::Relaxed);
+    }
+}
+
+impl const BitFieldSlot for usize {
+    #[inline(always)]
+    fn get(&self, field: BitField) -> usize {
+        let value = *self;
+        (value >> field.shift) & ((1usize << field.bits) - 1)
+    }
+
+    #[inline(always)]
+    fn set(&mut self, field: BitField, value: usize) {
+        let old_value = *self;
+        let mask = ((1usize << field.bits) - 1) << field.shift;
+        let shifted_value = value << field.shift;
+        debug_assert!((shifted_value & !mask) == 0);
+        let new_value = (old_value & !mask) | (value << field.shift);
+        *self = new_value;
+    }
+
+    fn delta(&mut self, field: BitField, delta: isize) -> usize {
+        let old = self.get(field);
+        let new = if delta > 0 {
+            old + (delta as usize)
+        } else {
+            old - ((-delta) as usize)
+        };
+        self.set(field, new);
+        new
     }
 }
