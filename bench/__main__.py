b91524a02e4ab1272814eb4a3a4d46df0836e0ce
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2022-02-24 00:44:04 +0000

    Update benchmarks

--- bench/__main__.py
+++ bench/__main__.py
@@ -1,7 +1,7 @@
 from .benchmarks import BENCHMARK_SUITE
 import argparse
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description='Benchmark runner.\n  - example: python3 -m bench -a mi hd sys buddy -b cfrac espresso barnes lean larson -i 10', formatter_class=argparse.RawTextHelpFormatter)
 parser.add_argument('-a', '--algorithms', nargs='*', default=BENCHMARK_SUITE.algorithms)
 parser.add_argument('-b', '--benchmarks', nargs='*', default=[b.name for b in BENCHMARK_SUITE.benchmarks])
 parser.add_argument('-i', '--invocations', nargs='?', type=int, default=1)
