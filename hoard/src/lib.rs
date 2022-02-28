5387b848f25bf0aa92d4fbb768702da6568b4865
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2022-02-28 01:36:28 +0000

    add hoard plan

---
+++ hoard/src/lib.rs
@@ -0,0 +1,92 @@
+#![allow(incomplete_features)]
+#![feature(type_alias_impl_trait)]
+#![feature(thread_local)]
+#![feature(const_fn_fn_ptr_basics)]
+#![feature(core_intrinsics)]
+
+extern crate mallockit;
+
+use core::alloc::Layout;
+use mallockit::{
+    space::*,
+    space::{freelist_space::*, large_object_space::*},
+    util::*,
+    Mutator, Plan,
+};
+use std::intrinsics::likely;
+
+const FREELIST_SPACE: SpaceId = SpaceId::DEFAULT;
+const LARGE_OBJECT_SPACE: SpaceId = SpaceId::LARGE_OBJECT_SPACE;
+
+#[mallockit::plan]
+struct Hoard {
+    freelist_space: FreeListSpace,
+    large_object_space: LargeObjectSpace,
+}
+
+impl Plan for Hoard {
+    type Mutator = HoardMutator;
+
+    fn new() -> Self {
+        Self {
+            freelist_space: FreeListSpace::new(FREELIST_SPACE),
+            large_object_space: LargeObjectSpace::new(LARGE_OBJECT_SPACE),
+        }
+    }
+
+    #[inline(always)]
+    fn get_layout(ptr: Address) -> Layout {
+        debug_assert!(FREELIST_SPACE.contains(ptr) || LARGE_OBJECT_SPACE.contains(ptr));
+        if likely(FREELIST_SPACE.contains(ptr)) {
+            FreeListSpace::get_layout(ptr)
+        } else {
+            Self::get().large_object_space.get_layout::<Size4K>(ptr)
+        }
+    }
+}
+
+#[mallockit::mutator]
+struct HoardMutator {
+    freelist: FreeListAllocator,
+    los: LargeObjectAllocator<Size4K>,
+}
+
+impl HoardMutator {
+    const fn new() -> Self {
+        Self {
+            freelist: FreeListAllocator::new(
+                Lazy::new(|| &Self::plan().freelist_space),
+                FREELIST_SPACE,
+            ),
+            los: LargeObjectAllocator::new(Lazy::new(|| &Self::plan().large_object_space)),
+        }
+    }
+}
+
+impl Mutator for HoardMutator {
+    type Plan = Hoard;
+    const NEW: Self = Self::new();
+
+    #[inline(always)]
+    fn alloc(&mut self, layout: Layout) -> Option<Address> {
+        if likely(FreeListSpace::can_allocate(layout)) {
+            mallockit::stat::track_allocation(layout, false);
+            self.freelist.alloc(layout)
+        } else {
+            mallockit::stat::track_allocation(layout, true);
+            self.los.alloc(layout)
+        }
+    }
+
+    #[inline(always)]
+    fn dealloc(&mut self, ptr: Address) {
+        debug_assert!(FREELIST_SPACE.contains(ptr) || LARGE_OBJECT_SPACE.contains(ptr));
+        if likely(FREELIST_SPACE.contains(ptr)) {
+            mallockit::stat::track_deallocation(false);
+            self.freelist.dealloc(ptr)
+        } else {
+            mallockit::stat::track_deallocation(false);
+            self.los.dealloc(ptr)
+        }
+    }
+}
