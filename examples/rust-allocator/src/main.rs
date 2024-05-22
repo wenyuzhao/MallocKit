88379eea6dd35b013bb1f2141f24066375d8dd8b
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2024-05-22 19:55:08 +1000

    refactor

--- examples/rust-allocator/src/main.rs
+++ examples/rust-allocator/src/main.rs
@@ -7,7 +7,7 @@ static GLOBAL: Global = Global;
 
 fn main() -> anyhow::Result<()> {
     println!("Hello, world!");
-    let mut file = File::open("../Cargo.toml")?;
+    let mut file = File::open("../../Cargo.toml")?;
     let mut content = String::new();
     file.read_to_string(&mut content)?;
     let toml_file = toml::from_str::<toml::Value>(&content)?;
