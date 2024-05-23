7ff89cf13713f1f14e44d81c6b733c0b7f79052f
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2024-05-23 13:31:55 +1000

    Minor

--- mallockit/src/util/mem/size_class.rs
+++ mallockit/src/util/mem/size_class.rs
@@ -1,10 +1,12 @@
 use std::alloc::Layout;
 
+use crate::util::constants::LOG_MIN_ALIGNMENT;
+
 use super::layout_utils::LayoutUtils;
 
 #[repr(transparent)]
 #[derive(Debug, Clone, Copy)]
-pub struct SizeClass<const LOG_COVERAGE: u8 = 4>(pub u8);
+pub struct SizeClass<const LOG_COVERAGE: u8 = LOG_MIN_ALIGNMENT>(pub u8);
 
 impl<const LOG_COVERAGE: u8> SizeClass<LOG_COVERAGE> {
     pub const fn as_usize(self) -> usize {
