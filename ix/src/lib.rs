95d7dbe06281dc88e099d1fe2f7e91396f85be79
Wenyu Zhao <wenyuzhaox@gmail.com>
2024-07-09 22:02:09 +1000

    wip

---
+++ ix/src/lib.rs
@@ -0,0 +1,90 @@
+#![feature(thread_local)]
+#![feature(step_trait)]
+#![feature(allocator_api)]
+#![feature(const_mut_refs)]
+#![feature(const_trait_impl)]
+#![feature(effects)]
+
+extern crate mallockit;
+
+mod hoard_space;
+mod pool;
+mod super_block;
+
+use hoard_space::*;
+use mallockit::{
+    space::{large_object_space::*, *},
+    util::*,
+    Mutator, Plan,
+};
+
+const HOARD_SPACE: SpaceId = SpaceId::DEFAULT;
+const LARGE_OBJECT_SPACE: SpaceId = SpaceId::LARGE_OBJECT_SPACE;
+
+#[mallockit::plan]
+struct Hoard {
+    hoard_space: HoardSpace,
+    large_object_space: LargeObjectSpace,
+}
+
+impl Plan for Hoard {
+    type Mutator = HoardMutator;
+
+    fn new() -> Self {
+        Self {
+            hoard_space: HoardSpace::new(HOARD_SPACE),
+            large_object_space: LargeObjectSpace::new(LARGE_OBJECT_SPACE),
+        }
+    }
+
+    fn get_layout(ptr: Address) -> Layout {
+        debug_assert!(HOARD_SPACE.contains(ptr) || LARGE_OBJECT_SPACE.contains(ptr));
+        if HOARD_SPACE.contains(ptr) {
+            HoardSpace::get_layout(ptr)
+        } else {
+            Self::get().large_object_space.get_layout::<Size4K>(ptr)
+        }
+    }
+}
+
+#[mallockit::mutator]
+struct HoardMutator {
+    hoard: HoardAllocator,
+    los: LargeObjectAllocator<Size4K, { 1 << 31 }, { 16 << 20 }>,
+    _padding: [usize; 8],
+}
+
+impl Mutator for HoardMutator {
+    type Plan = Hoard;
+
+    fn new() -> Self {
+        Self {
+            hoard: HoardAllocator::new(&Self::plan().hoard_space, HOARD_SPACE),
+            los: LargeObjectAllocator::new(&Self::plan().large_object_space),
+            _padding: [0; 8],
+        }
+    }
+
+    #[inline(always)]
+    fn alloc(&mut self, layout: Layout) -> Option<Address> {
+        if HoardSpace::can_allocate(layout) {
+            mallockit::stat::track_allocation(layout, false);
+            self.hoard.alloc(layout)
+        } else {
+            mallockit::stat::track_allocation(layout, true);
+            self.los.alloc(layout)
+        }
+    }
+
+    #[inline(always)]
+    fn dealloc(&mut self, ptr: Address) {
+        debug_assert!(HOARD_SPACE.contains(ptr) || LARGE_OBJECT_SPACE.contains(ptr));
+        if HOARD_SPACE.contains(ptr) {
+            mallockit::stat::track_deallocation(false);
+            self.hoard.dealloc(ptr)
+        } else {
+            mallockit::stat::track_deallocation(false);
+            self.los.dealloc(ptr)
+        }
+    }
+}
---
+++ ix/src/super_block.rs
@@ -0,0 +1,106 @@
+use std::{
+    num::NonZeroUsize,
+    ops::{Deref, DerefMut},
+};
+
+use mallockit::{space::page_resource::MemRegion, util::mem::size_class::SizeClass};
+
+use crate::pool::Pool;
+
+use super::Address;
+
+#[repr(C)]
+pub struct BlockMeta {
+    bump_cursor: u32,
+    used_bytes: u32,
+    pub prev: Option<SuperBlock>,
+    pub next: Option<SuperBlock>,
+    pub size_class: SizeClass,
+    pub group: u8,
+    head_cell: Address,
+    pub owner: &'static Pool,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct SuperBlock(NonZeroUsize);
+
+impl MemRegion for SuperBlock {
+    type Meta = BlockMeta;
+
+    const LOG_BYTES: usize = 18;
+
+    fn start(&self) -> Address {
+        Address::from(self.0.get())
+    }
+
+    fn from_address(addr: Address) -> Self {
+        debug_assert!(!addr.is_zero());
+        debug_assert!(Self::is_aligned(addr));
+        Self(unsafe { NonZeroUsize::new_unchecked(usize::from(addr)) })
+    }
+}
+
+impl Deref for SuperBlock {
+    type Target = BlockMeta;
+
+    fn deref(&self) -> &Self::Target {
+        self.meta()
+    }
+}
+
+impl DerefMut for SuperBlock {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe { self.meta_mut() }
+    }
+}
+
+impl SuperBlock {
+    pub fn init(mut self, _local: &'static Pool, size_class: SizeClass) {
+        debug_assert_eq!(Self::META_BYTES, Address::BYTES * 8);
+        self.size_class = size_class;
+        let size = size_class.bytes();
+        self.head_cell = Address::ZERO;
+        self.bump_cursor = (Address::ZERO + Self::META_BYTES).align_up(size).as_usize() as u32;
+        self.used_bytes = 0;
+    }
+
+    pub const fn used_bytes(self) -> usize {
+        self.used_bytes as _
+    }
+
+    pub const fn is_empty(self) -> bool {
+        self.used_bytes == 0
+    }
+
+    pub const fn is_full(self) -> bool {
+        self.bump_cursor >= Self::BYTES as u32 && self.head_cell.is_zero()
+    }
+
+    pub fn alloc_cell(&mut self) -> Option<Address> {
+        let cell = if self.bump_cursor < Self::BYTES as u32 {
+            let cell = self.start() + (self.bump_cursor as usize);
+            self.bump_cursor += self.size_class.bytes() as u32;
+            self.used_bytes += self.size_class.bytes() as u32;
+            return Some(cell);
+        } else if !self.head_cell.is_zero() {
+            self.head_cell
+        } else {
+            return None;
+        };
+        self.head_cell = unsafe { cell.load::<Address>() };
+        self.used_bytes += self.size_class.bytes() as u32;
+        Some(cell)
+    }
+
+    pub const fn free_cell(&mut self, cell: Address) {
+        unsafe {
+            cell.store(self.head_cell);
+        }
+        self.head_cell = cell;
+        self.used_bytes -= self.size_class.bytes() as u32;
+    }
+
+    pub fn is_owned_by(self, owner: &Pool) -> bool {
+        std::ptr::eq(self.owner, owner)
+    }
+}
